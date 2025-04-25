1ce6fa101f0a6ea633c5cd975acad6745ae8094b
Oskar Enmalm <oskarenmalm@gmail.com>
2025-04-26 00:27:12 +0200

    Initial commit

---
+++ src/main/java/hostlund/com/demo_spring_webapp/DemoSpringWebappApplication.java
@@ -0,0 +1,13 @@
+package hostlund.com.demo_spring_webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringWebappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hostlund/com/demo_spring_webapp/DemoSpringWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package hostlund.com.demo_spring_webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
