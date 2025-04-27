7b9cbf778bbce6e4491b4f6c8290910ed6fde7ea
Oskar Höstlund <oskarenmalm@gmail.com>
2025-04-27 03:54:24 +0200

    Got the h2 console loaded
    Can stop with the lookups in the code (this was also breaking though when two-way mapping was happening - article added on list of learnings todo

--- src/main/java/hostlund/com/demo_spring_webapp/bootstrap/DataBootstrap.java
+++ src/main/java/hostlund/com/demo_spring_webapp/bootstrap/DataBootstrap.java
@@ -26,20 +26,11 @@ public class DataBootstrap implements CommandLineRunner {
   }
 
   @Override
-  public void run(String... args) throws Exception {
+  public void run(String... args) {
     createArthurWithTwoBooks();
     createTwoAuthorsSameBook();
 
     createPublisher();
-
-    System.out.println("Author Count: " + authorRepository.count());
-    authorRepository.findAll().forEach(System.out::println);
-
-    System.out.println("Book Count: " + bookRepository.count());
-    bookRepository.findAll().forEach(System.out::println);
-
-    System.out.println("Publisher Count: " + publisherRepository.count());
-    publisherRepository.findAll().forEach(System.out::println);
   }
 
   private void createPublisher() {
@@ -54,8 +45,6 @@ public class DataBootstrap implements CommandLineRunner {
 
     publisher.getPublishedTitles().add(dataIntensive);
     publisherRepository.save(publisher);
-
-
   }
 
   private void createTwoAuthorsSameBook() {
@@ -65,19 +54,15 @@ public class DataBootstrap implements CommandLineRunner {
     Author david = new Author("David Thomas");
     david = authorRepository.save(david);
 
-    // TODO check what happens if they are not saved entities but they are added to the book
-    //  (which is then saved)
-    bookRepository.save(new Book("The Pragmatic Programmer", "99999",
+    Book pragmatic = bookRepository.save(new Book("The Pragmatic Programmer", "99999",
         Stream.of(andrew, david).collect(
             Collectors.toSet())));
 
-    // The mapping is (currently) not both ways, adding author to a book does not add the book to
-    // the author
-    // andrew.getBooks().add(pragmatic);
-    //authorRepository.save(andrew);
+    andrew.getBooks().add(pragmatic);
+    authorRepository.save(andrew);
 
-    //david.getBooks().add(pragmatic);
-    //authorRepository.save(david);
+    david.getBooks().add(pragmatic);
+    authorRepository.save(david);
   }
 
   private void createArthurWithTwoBooks() {
@@ -90,19 +75,13 @@ public class DataBootstrap implements CommandLineRunner {
     hound.setIsbn("1111");
     hound = bookRepository.save(hound);
     arthurSaved.getBooks().add(hound);
-    authorRepository.save(arthur);
+    arthurSaved = authorRepository.save(arthur);
 
-    //The author was empty even when the books had been added to the author
-    //hound.getAuthors().add(arthurSaved);
-    //bookRepository.save(hound);
-    //sherlock.getAuthors().add(arthurSaved);
-    //bookRepository.save(sherlock);
-
-    // The comment above is also why I believe this step is failing:
     //Unique index or primary key violation: "PUBLIC.PRIMARY_KEY_E ON PUBLIC.AUTHOR_BOOKS
-//    Book sherlock = new Book();
+//   Book sherlock = new Book();
 //    sherlock.setTitle("The Adventures of Sherlock Holmes");
 //    sherlock.setIsbn("2222");
+//    sherlock.getAuthors().add(arthurSaved);
 //    sherlock = bookRepository.save(sherlock);
 //    arthurSaved.getBooks().add(sherlock);
 //    arthurSaved = authorRepository.save(arthur);
